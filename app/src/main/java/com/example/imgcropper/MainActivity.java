2b99d8747cd4c1a14ccc0b035719ca08bafeb111
Kanika Sharma <kan.sharma66@gmail.com>
2023-05-20 23:00:52 +0530

    Image Editor

---
+++ app/src/main/java/com/example/imgcropper/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.imgcropper;
+
+
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img_view;
+
+    ActivityResultLauncher<String> mGetContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img_view=findViewById(R.id.img_view);
+        img_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContent.launch("image/*");
+            }
+        });
+        mGetContent=registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                Intent intent=new Intent(MainActivity.this,CropperActivity.class);
+                intent.putExtra("DATA",result.toString());
+                startActivityForResult(intent,101);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==-1 && requestCode==101){
+            String result=data.getStringExtra("RESULT");
+            Uri resultUri=null;
+            if(result!=null){
+                resultUri=Uri.parse(result);
+            }
+            img_view.setImageURI(resultUri);
+        }
+    }
+}
